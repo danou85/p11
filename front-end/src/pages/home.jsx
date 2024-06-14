223a0b2e3d8f04a53a6e06948b6806080cfb51fd
danou85 <111521806+danou85@users.noreply.github.com>
2024-06-14 19:55:43 +0200

    ajout page d'acceuil

--- front-end/src/pages/home.jsx
+++ front-end/src/pages/home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Logo from "../composant/logo";
+import Hero from "../composant/hero";
+import Features from "../composant/featuer";
 
 
 const Home = () => {
   return (
     <div>
       <Logo />
+      <Hero/>
+      < Features/>
     </div>
   );
 };
