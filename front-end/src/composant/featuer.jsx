bdea22746aff2cd91a8b9921eeb10c98dbac6752
danou85 <111521806+danou85@users.noreply.github.com>
2024-09-24 18:27:48 +0200

    Modification  feature

--- front-end/src/composant/featuer.jsx
+++ front-end/src/composant/featuer.jsx
@@ -1,49 +1,35 @@
 import React from 'react';
-import chatIcon from './icon-chat.png';
-import moneyIcon from './icon-money.png';
-import securityIcon from './icon-security.png';
-import "./main.css"; // Importez votre fichier main.css en utilisant un chemin relatif
+import chatIcon from './icon-chat.webp';   // Image au format webp
+import moneyIcon from './icon-money.webp'; // Assurez-vous que cette image est bien en .webp
+import securityIcon from './icon-security.webp';
+import "./main.css"; // Importez votre fichier main.css
+
+// Import du composant réutilisable FeatureItem
+import FeatureItem from './featureItem';
 
 const Features = () => {
   return (
     <section className="features">
       {/* Élément de fonctionnalité pour le chat */}
-      <div className="feature-item">
-        {/* Icône de fonctionnalité pour le chat */}
-        <img src={chatIcon} alt="Chat Icon" className="feature-icon" />
-        {/* Titre de l'élément de fonctionnalité */}
-        <h3 className="feature-item-title">You are our #1 priority</h3>
-        {/* Description de la fonctionnalité */}
-        <p>
-          Need to talk to a representative? You can get in touch through our
-          24/7 chat or through a phone call in less than 5 minutes.
-        </p>
-      </div>
+      <FeatureItem 
+        icon={chatIcon}
+        title="You are our #1 priority"
+        description="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."
+      />
       
       {/* Élément de fonctionnalité pour les économies */}
-      <div className="feature-item">
-        {/* Icône de fonctionnalité pour les économies */}
-        <img src={moneyIcon} alt="Money Icon" className="feature-icon" />
-        {/* Titre de l'élément de fonctionnalité */}
-        <h3 className="feature-item-title">More savings means higher rates</h3>
-        {/* Description de la fonctionnalité */}
-        <p>
-          The more you save with us, the higher your interest rate will be!
-        </p>
-      </div>
+      <FeatureItem 
+        icon={moneyIcon}
+        title="More savings means higher rates"
+        description="The more you save with us, the higher your interest rate will be!"
+      />
       
       {/* Élément de fonctionnalité pour la sécurité */}
-      <div className="feature-item">
-        {/* Icône de fonctionnalité pour la sécurité */}
-        <img src={securityIcon} alt="Security Icon" className="feature-icon" />
-        {/* Titre de l'élément de fonctionnalité */}
-        <h3 className="feature-item-title">Security you can trust</h3>
-        {/* Description de la fonctionnalité */}
-        <p>
-          We use top of the line encryption to make sure your data and money
-          is always safe.
-        </p>
-      </div>
+      <FeatureItem 
+        icon={securityIcon}
+        title="Security you can trust"
+        description="We use top of the line encryption to make sure your data and money is always safe."
+      />
     </section>
   );
 };
